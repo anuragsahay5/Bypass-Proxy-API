2d4f934e26c5388e7d58bdc6c99336ddc8221c25
anuragsahay5 <anuragtbg@gmail.com>
2023-07-14 18:14:37 +0530

    main-commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const app = express();
+const port = 3000;
+dotenv.config();
+app.use(cors());
+
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: process.env.SECRET_URL,
+    changeOrigin: true,
+    pathRewrite: { "^/api": "" },
+  })
+);
+
+app.get("/", (req, res) => res.send("Hello dunia!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
